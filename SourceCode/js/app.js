e6f47a1eeb613e3ae329b34a1b5a134bae7cec80
pallavidesai <s.pallavidesai@gmail.com>
2019-03-02 22:16:55 -0600

    rx

--- SourceCode/js/app.js
+++ SourceCode/js/app.js
@@ -1,26 +1,22 @@
-
-'use strict';
-
-// Declare app level module which depends on views, and components
 angular.module('myApp', [])
     .controller('ViewCtr', function ($scope, $http) {
         $scope.getPharmacy = function () {
+			// Getting values from view using ng model
             var latitude = $scope.lat
             var longitude =$scope.lon
             var dataParams = latitude.toString() + "," + longitude.toString();
-
-            if (latitude != null && latitude != "") {
+            if (latitude != null && latitude !=""  && longitude != null && longitude != "") {
+					// Calling API
                     $http.get('http://127.0.0.1:5000/getData?dataParams='+dataParams).then(function(result)
                         {
-                            var outputdata=result.data;
-                            console.log("the length of the data is  :"+outputdata.length);
-                            console.log("The values are:-"+JSON.stringify(outputdata) );
+							// Sending result from API to view using {}
+							var outputdata = result.data;
                             $scope.Name = outputdata["Name"];
                             $scope.Address = outputdata["Address"];
                             $scope.Distance = outputdata["Distance"];
                         },function(err)
                         {
-                            alert("failed");
+							alert("Server failed");
                             console.log(err);
                         })
             }
--- SourceCode/server.js
+++ SourceCode/server.js
@@ -5,19 +5,21 @@ var cors = require('cors');
 var app = express();
 var request=require("request");
 
+// My mlab url
 var url = 'mongodb://pallavi:pallavi123@ds135993.mlab.com:35993/asetrial1';
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
 app.get('/getData', function (req, res) {
+	// Parameters send by Angular 
     var dataparam = req.query.dataParams;
+	// Splitting with comma
     var array1 = dataparam.split(",");
     var latitude = array1[0];
-    //console.log("latitude"+latitude);
     var longitude = array1[1];
+	// Connecting to mlab
     MongoClient.connect(url,{ useNewUrlParser: true }, function(err, client) {
         if(err)
         {
@@ -26,9 +28,9 @@ app.get('/getData', function (req, res) {
         }
         if (err) throw err;
         var details, details_pos;
+		// DB name is asetrial1 and collection name is rx
         var db = client.db("asetrial1");
         db.collection("rx").find().toArray(function(err, result) {
-            //console.log(result)
             if (err) throw err;
             var i= 0, min =0;
             for(i = 0; i < result.length; i++) {
@@ -39,20 +41,23 @@ app.get('/getData', function (req, res) {
                 var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
                 var d = R * c; // Distance in km
                 console.log("value of d is" + c +" "+ a );
+				// Calculating minimum
                 if((i == 0) || (min > d)) {
                     min = d;
                     console.log("minimun is"+ min);
                     details_pos = i;
                 }
             }
+			// Converting KMs to Miles
             min = min/1.609;
+			// Packet to be sent
             details =  {
                 "Name": result[details_pos].name,
                 "Address": result[details_pos].address,
                 "Distance": min
             };
-            console.log( details);
             client.close();
+			// Sending to client
             res.json(details);
         });
     });
@@ -62,6 +67,7 @@ function deg2rad(deg) {
     return deg * (Math.PI/180)
 }
 
+// Our server. Listening to port 5000
 var server = app.listen(5000,function () {
     var host = server.address().address
     var port = server.address().port
